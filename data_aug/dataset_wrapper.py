db77088f8be854e430e5c56cafe5d4c78c70cc5c
dy0607 <dy0607@foxmail.com>
2020-12-02 23:23:46 +0800

    add clustering

--- data_aug/dataset_wrapper.py
+++ data_aug/dataset_wrapper.py
@@ -7,65 +7,91 @@ from torchvision import datasets
 
 np.random.seed(0)
 
-
 class DataSetWrapper(object):
 
-    def __init__(self, batch_size, num_workers, valid_size, input_shape, s):
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        self.valid_size = valid_size
-        self.s = s
-        self.input_shape = eval(input_shape)
+	def __init__(self, batch_size, num_workers, valid_size, input_shape, s):
+		self.batch_size = batch_size
+		self.num_workers = num_workers
+		self.valid_size = valid_size
+		self.s = s
+		self.input_shape = eval(input_shape)
 
-    def get_data_loaders(self):
-        data_augment = self._get_simclr_pipeline_transform()
+	def get_data_loaders(self):
 
-		# use CIFAR10
-        train_dataset = datasets.CIFAR10('./data', train=True, download=True,
-                                       transform=SimCLRDataTransform(data_augment))
+		data_augment, base_transform = self._get_simclr_pipeline_transforms()
+		
+		# train dataset, for contrastive learning
+		train_dataset = datasets.CIFAR10('./data', train=True, download=True,
+									   transform=SimCLRDataTransform(data_augment, base_transform))
 
-        train_loader, valid_loader = self.get_train_validation_data_loaders(train_dataset)
-        return train_loader, valid_loader
+		# the original dataset, for clustering
+		original_dataset = datasets.CIFAR10('./data', train=True, download=True,
+									   transform=base_transform)
 
-    def _get_simclr_pipeline_transform(self):
-        # get a set of data augmentation transformations as described in the SimCLR paper.
-        color_jitter = transforms.ColorJitter(0.8 * self.s, 0.8 * self.s, 0.8 * self.s, 0.2 * self.s)
+		train_sampler, valid_sampler = self.get_train_validation_samplers(len(train_dataset))
+
+		clustering_loader = self._get_data_loader(original_dataset, train_sampler)
+		train_loader = self._get_data_loader(train_dataset, train_sampler)
+		valid_loader = self._get_data_loader(train_dataset, valid_sampler)
+		
+		return train_loader, valid_loader, clustering_loader
+	
+	def _get_data_loader(self, dataset, sampler):
+
+		dataloader = DataLoader(dataset, batch_size=self.batch_size, sampler=sampler,
+								num_workers=self.num_workers, drop_last=True, shuffle=False)
+
+		return dataloader
+
+
+	def _get_simclr_pipeline_transforms(self):
+		# get a set of data augmentation transformations as described in the SimCLR paper.
+		color_jitter = transforms.ColorJitter(0.8 * self.s, 0.8 * self.s, 0.8 * self.s, 0.2 * self.s)
 
 		# for CIFAR10, leave out the gaussian blur
-        data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=self.input_shape[0]),
-                                              transforms.RandomHorizontalFlip(),
-                                              transforms.RandomApply([color_jitter], p=0.8),
-                                              transforms.RandomGrayscale(p=0.2),
-                                              # GaussianBlur(kernel_size=int(0.1 * self.input_shape[0])),
-                                              transforms.ToTensor()])
-        return data_transforms
+		data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=self.input_shape[0]),
+											  transforms.RandomHorizontalFlip(),
+											  transforms.RandomApply([color_jitter], p=0.8),
+											  transforms.RandomGrayscale(p=0.2),
+											  # GaussianBlur(kernel_size=int(0.1 * self.input_shape[0])),
+											  transforms.ToTensor()])
 
-    def get_train_validation_data_loaders(self, train_dataset):
-        # obtain training indices that will be used for validation
-        num_train = len(train_dataset)
-        indices = list(range(num_train))
-        np.random.shuffle(indices)
+		# base transform, used for clustering
+		base_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+										  # transforms.RandomApply([color_jitter], p=0.8),
+										  # transforms.RandomGrayscale(p=0.2),
+										  # GaussianBlur(kernel_size=int(0.1 * self.input_shape[0])),
+										  transforms.ToTensor()])
 
-        split = int(np.floor(self.valid_size * num_train))
-        train_idx, valid_idx = indices[split:], indices[:split]
 
-        # define samplers for obtaining training and validation batches
-        train_sampler = SubsetRandomSampler(train_idx)
-        valid_sampler = SubsetRandomSampler(valid_idx)
+		return data_transforms, base_transform
 
-        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, sampler=train_sampler,
-                                  num_workers=self.num_workers, drop_last=True, shuffle=False)
+	def get_train_validation_samplers(self, num_train):
+		# obtain training indices that will be used for validation
+		indices = list(range(num_train))
+		np.random.shuffle(indices)
 
-        valid_loader = DataLoader(train_dataset, batch_size=self.batch_size, sampler=valid_sampler,
-                                  num_workers=self.num_workers, drop_last=True)
-        return train_loader, valid_loader
+		split = int(np.floor(self.valid_size * num_train))
+		train_idx, valid_idx = indices[split:], indices[:split]
 
+		# define samplers for obtaining training and validation batches
+		train_sampler = SubsetRandomSampler(train_idx)
+		valid_sampler = SubsetRandomSampler(valid_idx)
+
+		return train_sampler, valid_sampler
 
 class SimCLRDataTransform(object):
-    def __init__(self, transform):
-        self.transform = transform
+	def __init__(self, transform, base):
+		self.transform = transform
+		self.base = base
 
-    def __call__(self, sample):
-        xi = self.transform(sample)
-        xj = self.transform(sample)
-        return xi, xj
+	def __call__(self, sample):
+
+		# positive pair
+		xi = self.transform(sample)
+		xj = self.transform(sample)
+
+		# base image
+		x = self.base(sample)
+
+		return x, xi, xj
--- simclr.py
+++ simclr.py
@@ -3,19 +3,22 @@ from models.resnet_simclr import ResNetSimCLR
 from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
 from loss.nt_xent import NTXentLoss
+
+from sklearn.cluster import KMeans, MiniBatchKMeans
+
 import os
 import shutil
 import sys
 
 apex_support = False
 try:
-    sys.path.append('./apex')
-    from apex import amp
+	sys.path.append('./apex')
+	from apex import amp
 
-    apex_support = True
+	apex_support = True
 except:
-    print("Please install apex for mixed precision training from: https://github.com/NVIDIA/apex")
-    apex_support = False
+	print("Please install apex for mixed precision training from: https://github.com/NVIDIA/apex")
+	apex_support = False
 
 import numpy as np
 
@@ -23,129 +26,203 @@ torch.manual_seed(0)
 
 
 def _save_config_file(model_checkpoints_folder):
-    if not os.path.exists(model_checkpoints_folder):
-        os.makedirs(model_checkpoints_folder)
-        shutil.copy('./config.yaml', os.path.join(model_checkpoints_folder, 'config.yaml'))
+	if not os.path.exists(model_checkpoints_folder):
+		os.makedirs(model_checkpoints_folder)
+		shutil.copy('./config.yaml', os.path.join(model_checkpoints_folder, 'config.yaml'))
 
 
 class SimCLR(object):
 
-    def __init__(self, dataset, config):
-        self.config = config
-        self.device = self._get_device()
-        self.writer = SummaryWriter()
-        self.dataset = dataset
-        self.nt_xent_criterion = NTXentLoss(self.device, config['batch_size'], **config['loss'])
+	def __init__(self, dataset, config):
+		self.config = config
+		self.device = self._get_device()
+		self.writer = SummaryWriter()
+		self.dataset = dataset
+		
+		self.criterion = torch.nn.KLDivLoss(reduction="batchmean")
+		self.nt_xent_criterion = NTXentLoss(self.device, config['batch_size'], **config['loss'])
 
-    def _get_device(self):
-        device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
-        print("Running on:", device)
-        return device
+	def _get_device(self):
+		device = self.config['device'] if torch.cuda.is_available() else 'cpu'
+		print("Running on:", device)
+		return device
 
-    def _step(self, model, xis, xjs, n_iter):
+	def _step(self, model, x, xis, xjs, n_iter):
 
-        # get the representations and the projections
-        ris, zis = model(xis)  # [N,C]
+		# get the representations and the projections
+		ris, zis = model(xis)
+		rjs, zjs = model(xjs)
+		rx, px = model(x)
 
-        # get the representations and the projections
-        rjs, zjs = model(xjs)  # [N,C]
+		# normalize projection feature vectors
+		zis = F.normalize(zis, dim=1)
+		zjs = F.normalize(zjs, dim=1)
+		px = F.normalize(px, dim=1)
 
-        # normalize projection feature vectors
-        zis = F.normalize(zis, dim=1)
-        zjs = F.normalize(zjs, dim=1)
+		labels = F.softmax(torch.matmul(px, self.C), dim=1)
+		pi = F.log_softmax(torch.matmul(zis, self.C), dim=1)
+		pj = F.log_softmax(torch.matmul(zjs, self.C), dim=1)
+		# print(pi, pj, labels)
+		
+		loss1 = self.criterion(pi, labels) + self.criterion(pj, labels)
+		loss2 = self.nt_xent_criterion(zis, zjs)
 
-        loss = self.nt_xent_criterion(zis, zjs)
-        return loss
+		# print("x", loss1)
+		# print(loss2)
+		# exit()
 
-    def train(self):
+		return loss1 + loss2
 
-        train_loader, valid_loader = self.dataset.get_data_loaders()
+	def _get_clustering(self, model, dataloader):
 
-        model = ResNetSimCLR(**self.config["model"]).to(self.device)
-        model = self._load_pre_trained_weights(model)
+		with torch.no_grad():
 
-        optimizer = torch.optim.Adam(model.parameters(), 3e-4, weight_decay=eval(self.config['weight_decay']))
+			print('Clustering...')
 
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.config['epochs'], eta_min=0,
-                                                               last_epoch=-1)
+			kmeans = 0
+			score = 0
 
-        if apex_support and self.config['fp16_precision']:
-            model, optimizer = amp.initialize(model, optimizer,
-                                              opt_level='O2',
-                                              keep_batchnorm_fp32=True)
+			if not self.config['minibatch_kmeans']:
+			
+				features = []
+				for x, _ in dataloader:
+					x = x.to(self.device)
+					_, projections = model(x)
+					new_features = F.normalize(projections, dim=1).cpu().numpy()
+					features.append(new_features)
+					
+				features = np.concatenate(features, axis=0)
+				kmeans = KMeans(n_clusters=self.config['n_clusters']).fit(features)
+				score = kmeans.score(features)
 
-        model_checkpoints_folder = os.path.join(self.writer.log_dir, 'checkpoints')
+			else:
 
-        # save config file
-        _save_config_file(model_checkpoints_folder)
+				kmeans = MiniBatchKMeans(n_clusters=self.config['n_clusters'], 
+										batch_size=self.config['batch_size'])
+				for x, _ in dataloader:
+					x = x.to(self.device)
+					_, projections = model(x)
+					features = F.normalize(projections, dim=1).cpu().numpy()
+					kmeans.partial_fit(features)
+					score += kmeans.score(features)
+			
+			centers = torch.tensor(kmeans.cluster_centers_)
+			centers = F.normalize(centers, dim=1).to(self.device)
+			self.C = centers.transpose_(0, 1)
+			
+			count = [0 for i in range(self.config['n_clusters'])]
 
-        n_iter = 0
-        valid_n_iter = 0
-        best_valid_loss = np.inf
+			for i in range(len(kmeans.labels_)):
+				count[kmeans.labels_[i]] += 1
 
-        for epoch_counter in range(self.config['epochs']):
-            for (xis, xjs), _ in train_loader:
-                optimizer.zero_grad()
+			self.writer.add_scalar('clustering score', score, global_step=self.cluster_n_iter)
+			self.cluster_n_iter += 1
 
-                xis = xis.to(self.device)
-                xjs = xjs.to(self.device)
+			print(count)
+			print(self.C)
 
-                loss = self._step(model, xis, xjs, n_iter)
+	def train(self):
 
-                if n_iter % self.config['log_every_n_steps'] == 0:
-                    self.writer.add_scalar('train_loss', loss, global_step=n_iter)
+		train_loader, valid_loader, clustering_loader = self.dataset.get_data_loaders()
 
-                if apex_support and self.config['fp16_precision']:
-                    with amp.scale_loss(loss, optimizer) as scaled_loss:
-                        scaled_loss.backward()
-                else:
-                    loss.backward()
+		model = ResNetSimCLR(**self.config["model"]).to(self.device)
+		model = self._load_pre_trained_weights(model)
 
-                optimizer.step()
-                n_iter += 1
+		optimizer = torch.optim.Adam(model.parameters(), 3e-4, weight_decay=eval(self.config['weight_decay']))
+		scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.config['epochs'], eta_min=0,
+															   last_epoch=-1)
 
-            # validate the model if requested
-            if epoch_counter % self.config['eval_every_n_epochs'] == 0:
-                valid_loss = self._validate(model, valid_loader)
-                if valid_loss < best_valid_loss:
-                    # save the model weights
-                    best_valid_loss = valid_loss
-                    torch.save(model.state_dict(), os.path.join(model_checkpoints_folder, 'model.pth'))
+		if apex_support and self.config['fp16_precision']:
+			model, optimizer = amp.initialize(model, optimizer,
+											  opt_level='O2',
+											  keep_batchnorm_fp32=True)
 
-                self.writer.add_scalar('validation_loss', valid_loss, global_step=valid_n_iter)
-                valid_n_iter += 1
+		model_checkpoints_folder = os.path.join(self.writer.log_dir, 'checkpoints')
 
-            # warmup for the first 10 epochs
-            if epoch_counter >= 10:
-                scheduler.step()
-            self.writer.add_scalar('cosine_lr_decay', scheduler.get_lr()[0], global_step=n_iter)
+		# save config file
+		_save_config_file(model_checkpoints_folder)
 
-    def _load_pre_trained_weights(self, model):
-        try:
-            checkpoints_folder = os.path.join('./runs', self.config['fine_tune_from'], 'checkpoints')
-            state_dict = torch.load(os.path.join(checkpoints_folder, 'model.pth'))
-            model.load_state_dict(state_dict)
-            print("Loaded pre-trained model with success.")
-        except FileNotFoundError:
-            print("Pre-trained weights not found. Training from scratch.")
+		# reset the counters
+		self.n_iter = 0
+		valid_n_iter = 0
 
-        return model
+		self.cluster_n_iter = 0
+		best_valid_loss = np.inf
 
-    def _validate(self, model, valid_loader):
+		for epoch_counter in range(self.config['epochs']):
 
-        # validation steps
-        with torch.no_grad():
-            model.eval()
+			if epoch_counter % self.config['cluster_every_n_epochs'] == 0:
+				self._get_clustering(model, clustering_loader)
 
-            valid_loss = 0.0
-            counter = 0
-            for (xis, xjs), _ in valid_loader:
-                xis = xis.to(self.device)
-                xjs = xjs.to(self.device)
+			self._train(model, optimizer, train_loader)
 
-                loss = self._step(model, xis, xjs, counter)
-                valid_loss += loss.item()
-                counter += 1
-            valid_loss /= counter
-        model.train()
-        return valid_loss
+			# validate the model if requested
+			if epoch_counter % self.config['eval_every_n_epochs'] == 0:
+				valid_loss = self._validate(model, valid_loader)
+				if valid_loss < best_valid_loss:
+					# save the model weights
+					best_valid_loss = valid_loss
+					torch.save(model.state_dict(), os.path.join(model_checkpoints_folder, 'model.pth'))
+
+				self.writer.add_scalar('validation_loss', valid_loss, global_step=valid_n_iter)
+				valid_n_iter += 1
+
+			# warmup for the first 10 epochs
+			if epoch_counter >= 10:
+				scheduler.step()
+			self.writer.add_scalar('cosine_lr_decay', scheduler.get_lr()[0], global_step=self.n_iter)
+
+	def _load_pre_trained_weights(self, model):
+		try:
+			checkpoints_folder = os.path.join('./runs', self.config['fine_tune_from'], 'checkpoints')
+			state_dict = torch.load(os.path.join(checkpoints_folder, 'model.pth'))
+			model.load_state_dict(state_dict)
+			print("Loaded pre-trained model with success.")
+		except FileNotFoundError:
+			print("Pre-trained weights not found. Training from scratch.")
+
+		return model
+
+	def _train(self, model, optimizer, train_loader):
+		for (x, xis, xjs), _ in train_loader:
+			
+			optimizer.zero_grad()
+
+			x = x.to(self.device)
+			xis = xis.to(self.device)
+			xjs = xjs.to(self.device)
+
+			loss = self._step(model, x, xis, xjs, self.n_iter)
+
+			if self.n_iter % self.config['log_every_n_steps'] == 0:
+				self.writer.add_scalar('train_loss', loss, global_step=self.n_iter)
+
+			if apex_support and self.config['fp16_precision']:
+				with amp.scale_loss(loss, optimizer) as scaled_loss:
+					scaled_loss.backward()
+			else:
+				loss.backward()
+
+			optimizer.step()
+			self.n_iter += 1
+
+	def _validate(self, model, valid_loader):
+
+		# validation steps
+		with torch.no_grad():
+			model.eval()
+
+			valid_loss = 0.0
+			counter = 0
+			for (x, xis, xjs), _ in valid_loader:
+				
+				x = x.to(self.device)
+				xis = xis.to(self.device)
+				xjs = xjs.to(self.device)
+
+				loss = self._step(model, x, xis, xjs, counter)
+				valid_loss += loss.item()
+				counter += 1
+			valid_loss /= counter
+		model.train()
+		return valid_loss
